a378d81caa20961a1fb83459aff43596c93d5a4b
tgeorge <tgeorge@clearwateranalytics.com>
2019-03-15 01:12:10 -0600

    Regex stuff

--- yamloutput.go
+++ yamloutput.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"log"
+	"regexp"
 )
 
 type Person struct {
@@ -24,4 +25,26 @@ func main() {
 		log.Fatalf("error: %v", err)
 	}
 	fmt.Println(string(y))
+
+	var node1 string = "prod-boi-service-group1-app1"
+	var node2 string = "puppetagent-adskafjlk-adsf"
+	var node3 string = "woot1"
+
+	fmt.Println(evalName(node1))
+	fmt.Println(evalName(node2))
+	fmt.Println(evalName(node3))
+}
+
+func evalName(nodeName string) (string, []string) {
+	reLinuxServer := regexp.MustCompile(`^([a-z0-9]+)-([a-z0-9]+)-([a-z0-9]+)-([a-z0-9]+)-([a-z]+)([0-9]+)`)
+	reKubernetes := regexp.MustCompile(`^([a-z0-9]+)-.*`)
+
+	switch {
+	case reLinuxServer.MatchString(nodeName):
+		return "LinuxServer", reLinuxServer.FindStringSubmatch(nodeName)
+	case reKubernetes.MatchString(nodeName):
+		return "Kubernetes", reKubernetes.FindStringSubmatch(nodeName)
+	default:
+		return "No Match", make([]string, 0)
+	}
 }
